f53e821f3bf203d649e9a42475d03b6d274d1c79
Matija Čupić <matteeyah@gmail.com>
2019-08-13 20:00:27 +0200

    Remove redundant provider column

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,7 @@
 
 ActiveRecord::Schema.define(version: 20_190_809_000_537) do
   create_table 'users', force: :cascade do |t|
-    t.string 'provider'
-    t.string 'uid'
+    t.string 'external_uid'
     t.string 'name'
     t.string 'email'
   end
