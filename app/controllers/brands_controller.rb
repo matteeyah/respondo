6e61136a74dad9e294cf009faf89dcdf421aadde
Matija Čupić <matteeyah@gmail.com>
2019-08-29 15:12:22 +0200

    Implement add_user and remove_user

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -3,20 +3,41 @@
 class BrandsController < ApplicationController
   include Pagy::Backend
 
-  before_action :brand, only: [:show]
-  before_action :brands, only: [:index]
+  before_action :brand, except: [:index]
+  before_action :user, only: %i[add_user remove_user]
+  before_action :authorize!, only: [:edit]
 
-  def index; end
-
-  def show; end
-
-  private
-
-  def brands
+  def index
     @pagy, @brands = pagy(Brand.all)
   end
 
+  def show; end
+
+  def edit; end
+
+  def add_user
+    @brand.users << @user
+  end
+
+  def remove_user
+    @brand.users.delete(@user)
+  end
+
+  private
+
   def brand
-    @brand ||= Brand.find(params[:id])
+    @brand ||= Brand.find(params[:id] || params[:brand_id])
+  end
+
+  def user
+    @user ||= User.find(params[:user_id])
+  end
+
+  def authorize!
+    return if @brand == @user_brand
+
+    redirect_to root_path,
+                status: :forbidden,
+                alert: 'You are not allowed to edit the Brand.'
   end
 end
--- app/helpers/brands_helper.rb
+++ app/helpers/brands_helper.rb
@@ -2,4 +2,12 @@
 
 module BrandsHelper
   include Pagy::Frontend
+
+  def remove_users_dropdown_options
+    @brand.users.map { |user| [user.name, user.id] }
+  end
+
+  def add_users_dropdown_options
+    User.where.not(id: @brand.users.pluck(:id)).map { |user| [user.name, user.id] }
+  end
 end
