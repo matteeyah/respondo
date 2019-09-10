998ed4ef53fa1d59cfdd096d3c78102594230c3b
Matija Čupić <matteeyah@gmail.com>
2019-09-10 15:45:33 +0200

    Remove unused #remove_users_dropdown_options

--- app/helpers/brands_helper.rb
+++ app/helpers/brands_helper.rb
@@ -3,10 +3,6 @@
 module BrandsHelper
   include Pagy::Frontend
 
-  def remove_users_dropdown_options
-    @brand.users.pluck(:name, :id)
-  end
-
   def add_users_dropdown_options
     User.not_in_brand(@brand.id).pluck(:name, :id)
   end
