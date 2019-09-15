f293cc730354b18dc26a2f48465d727be258dd3a
Matija Čupić <matteeyah@gmail.com>
2019-09-15 14:37:03 +0200

    Document bootstrap js import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// Required for use of bootstrap
 import 'bootstrap'
 
 require('@rails/ujs').start()
