5e7a384c86f77c8c39ef4d8aebe9282069100436
Matija Čupić <matteeyah@gmail.com>
2019-09-20 15:28:24 +0200

    Refactor LoadNewTweetsJob spec

--- spec/jobs/load_new_tweets_job_spec.rb
+++ spec/jobs/load_new_tweets_job_spec.rb
@@ -1,23 +1,22 @@
 # frozen_string_literal: true
 
 RSpec.describe LoadNewTweetsJob, type: :job do
-  let(:brand) { FactoryBot.create(:brand) }
-  let(:mentions) { [instance_double(Twitter::Tweet), instance_double(Twitter::Tweet)] }
-  let(:ticket_class) { class_spy(Ticket) }
-
   describe '#perform' do
     subject(:perform_now) { described_class.perform_now(brand.id) }
 
+    let(:mentions) { [instance_double(Twitter::Tweet), instance_double(Twitter::Tweet)] }
+    let(:brand) { instance_double(Brand, id: 123, new_mentions: mentions) }
+    let(:ticket_class) { class_spy(Ticket) }
+
     before do
       allow(Brand).to receive(:find_by).with(id: brand.id).and_return(brand)
-      allow(brand).to receive(:new_mentions).and_return(mentions)
       stub_const('Ticket', ticket_class)
     end
 
     it 'creates tickets' do
       perform_now
 
-      expect(ticket_class).to have_received(:create_from_tweet).with(anything, brand).twice
+      mentions.each { |mention| expect(ticket_class).to have_received(:create_from_tweet).with(mention, brand).once }
     end
   end
 end
