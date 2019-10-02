b4e85e859256e7c743bc41464f1269753574ae87
Matija Čupić <matteeyah@gmail.com>
2019-10-02 14:15:54 +0200

    Test both replying as brand and user

--- spec/controllers/brands/tickets_controller_spec.rb
+++ spec/controllers/brands/tickets_controller_spec.rb
@@ -35,49 +35,70 @@ RSpec.describe Brands::TicketsController, type: :controller do
   describe 'POST reply' do
     subject(:post_reply) { post :reply, params: { brand_id: brand.id, ticket_id: ticket.id, response_text: 'does not matter' } }
 
-    let!(:ticket) { FactoryBot.create(:ticket, brand: brand) }
+    let(:ticket) { FactoryBot.create(:ticket, brand: brand) }
 
-    let(:tweet) do
-      instance_double(Twitter::Tweet, id: '1', text: 'does not matter', in_reply_to_tweet_id: ticket.external_uid,
-                                      user: instance_double(Twitter::User, id: '2', screen_name: 'test'))
-    end
-
-    let(:client) do
-      instance_spy(Clients::Twitter, reply: tweet)
-    end
-
-    before do
-      allow(controller).to receive(:brand).and_return(brand)
-      allow(controller).to receive(:client).and_return(client)
-    end
-
-    context 'when user is authorized' do
+    context 'when authorized' do
       let(:user) { FactoryBot.create(:user) }
 
+      let(:tweet) do
+        instance_double(Twitter::Tweet, id: '1', text: 'does not matter', in_reply_to_tweet_id: ticket.external_uid,
+                        user: instance_double(Twitter::User, id: '2', screen_name: 'test'))
+      end
+
+      let(:client) do
+        instance_spy(Clients::Twitter, reply: tweet)
+      end
+
       before do
-        FactoryBot.create(:account, user: user)
-
-        allow(controller).to receive(:current_user).and_return(user)
-
-        brand.users << user
-
         sign_in(user)
       end
 
-      it 'replies to the ticket' do
-        post_reply
+      context 'when authorized as a brand' do
+        before do
+          brand.users << user
 
-        expect(client).to have_received(:reply).with('does not matter', ticket.external_uid)
+          allow(controller).to receive(:brand).and_return(brand)
+          allow(brand).to receive(:twitter).and_return(client)
+        end
+
+        it 'replies to the ticket' do
+          post_reply
+
+          expect(client).to have_received(:reply).with('does not matter', ticket.external_uid)
+        end
+
+        it 'creates a reply ticket' do
+          expect { post_reply }.to change(Ticket, :count).from(1).to(2)
+        end
+
+        it 'creates a reply ticket with matching attributes' do
+          post_reply
+
+          expect(Ticket.last).to have_attributes(parent: ticket, content: 'does not matter')
+        end
       end
 
-      it 'creates a reply ticket' do
-        expect { post_reply }.to change(Ticket, :count).from(1).to(2)
-      end
+      context 'when authorized as a user' do
+        before do
+          allow(controller).to receive(:current_user).and_return(user)
+          allow(user).to receive(:client_for_provider).and_return(client)
+        end
 
-      it 'creates a reply ticket with matching attributes' do
-        post_reply
+        it 'replies to the ticket' do
+          post_reply
 
-        expect(Ticket.last).to have_attributes(parent: ticket, content: 'does not matter')
+          expect(client).to have_received(:reply).with('does not matter', ticket.external_uid)
+        end
+
+        it 'creates a reply ticket' do
+          expect { post_reply }.to change(Ticket, :count).from(1).to(2)
+        end
+
+        it 'creates a reply ticket with matching attributes' do
+          post_reply
+
+          expect(Ticket.last).to have_attributes(parent: ticket, content: 'does not matter')
+        end
       end
     end
 
