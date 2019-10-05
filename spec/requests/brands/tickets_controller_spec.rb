ee3cf1f841d8725fc15d84d001d0f569824e4997
Matija Čupić <matteeyah@gmail.com>
2019-10-05 17:01:48 +0200

    Move Brands::TicketsController to request type

--- spec/requests/brands/tickets_controller_spec.rb
+++ spec/requests/brands/tickets_controller_spec.rb
@@ -2,13 +2,13 @@
 
 require './spec/support/sign_in_out_helpers.rb'
 
-RSpec.describe Brands::TicketsController, type: :controller do
+RSpec.describe Brands::TicketsController, type: :request do
   include SignInOutHelpers
 
   let(:brand) { FactoryBot.create(:brand) }
 
   describe 'GET index' do
-    subject(:get_index) { get :index, params: { brand_id: brand.id } }
+    subject(:get_index) { get "/brands/#{brand.id}/tickets" }
 
     before do
       FactoryBot.create(:ticket, status: :open, brand: brand)
@@ -33,21 +33,20 @@ RSpec.describe Brands::TicketsController, type: :controller do
   end
 
   describe 'POST reply' do
-    subject(:post_reply) { post :reply, params: { brand_id: brand.id, ticket_id: ticket.id, response_text: 'does not matter' } }
+    subject(:post_reply) { post "/brands/#{brand.id}/tickets/#{ticket.id}/reply", params: { response_text: 'does not matter' } }
 
     let(:ticket) { FactoryBot.create(:ticket, brand: brand) }
 
     context 'when authorized' do
-      let(:user) { FactoryBot.create(:user) }
+      let(:user) { FactoryBot.create(:user, :with_account) }
 
       let(:tweet) do
         instance_double(Twitter::Tweet, id: '1', text: 'does not matter', in_reply_to_tweet_id: ticket.external_uid,
                                         user: instance_double(Twitter::User, id: '2', screen_name: 'test'))
       end
 
-      let(:client) do
-        instance_spy(Clients::Twitter, reply: tweet)
-      end
+      let(:client) { instance_spy(Clients::Twitter, reply: tweet) }
+      let(:client_class) { class_spy(Clients::Twitter, new: client) }
 
       before do
         sign_in(user)
@@ -57,8 +56,7 @@ RSpec.describe Brands::TicketsController, type: :controller do
         before do
           brand.users << user
 
-          allow(controller).to receive(:brand).and_return(brand)
-          allow(brand).to receive(:twitter).and_return(client)
+          stub_const('Clients::Twitter', client_class)
         end
 
         it 'replies to the ticket' do
@@ -74,14 +72,15 @@ RSpec.describe Brands::TicketsController, type: :controller do
         it 'creates a reply ticket with matching attributes' do
           post_reply
 
-          expect(Ticket.last).to have_attributes(parent: ticket, content: 'does not matter')
+          expect(Ticket.find_by(external_uid: tweet.id)).to have_attributes(parent: ticket, content: 'does not matter')
         end
       end
 
       context 'when authorized as a user' do
         before do
-          allow(controller).to receive(:current_user).and_return(user)
-          allow(user).to receive(:client_for_provider).and_return(client)
+          FactoryBot.create(:account, provider: 'twitter', user: user)
+
+          stub_const('Clients::Twitter', client_class)
         end
 
         it 'replies to the ticket' do
@@ -97,7 +96,7 @@ RSpec.describe Brands::TicketsController, type: :controller do
         it 'creates a reply ticket with matching attributes' do
           post_reply
 
-          expect(Ticket.last).to have_attributes(parent: ticket, content: 'does not matter')
+          expect(Ticket.find_by(external_uid: tweet.id)).to have_attributes(parent: ticket, content: 'does not matter')
         end
       end
     end
@@ -106,7 +105,7 @@ RSpec.describe Brands::TicketsController, type: :controller do
       it 'sets the flash' do
         post_reply
 
-        expect(controller).to set_flash[:alert].to('You are not allowed to reply to the ticket.')
+        expect(controller.flash[:alert]).to eq('You are not allowed to reply to the ticket.')
       end
 
       it 'redirects the user' do
@@ -116,17 +115,12 @@ RSpec.describe Brands::TicketsController, type: :controller do
   end
 
   describe 'POST invert_status' do
-    subject(:post_invert_status) { post :invert_status, params: { brand_id: brand.id, ticket_id: ticket.id } }
+    subject(:post_invert_status) { post "/brands/#{brand.id}/tickets/#{ticket.id}/invert_status" }
 
     let(:ticket) { FactoryBot.create(:ticket) }
 
-    before do
-      allow(controller).to receive(:brand).and_return(brand)
-      allow(controller).to receive(:ticket).and_return(ticket)
-    end
-
     context 'when user is authorized' do
-      let(:user) { FactoryBot.create(:user) }
+      let(:user) { FactoryBot.create(:user, :with_account) }
 
       before do
         brand.users << user
@@ -136,21 +130,21 @@ RSpec.describe Brands::TicketsController, type: :controller do
 
       context 'when the ticket is open' do
         before do
-          ticket.status = 'open'
+          ticket.update(status: 'open')
         end
 
         it 'solves the ticket' do
-          expect { post_invert_status }.to change(ticket, :status).from('open').to('solved')
+          expect { post_invert_status }.to change { ticket.reload.status }.from('open').to('solved')
         end
       end
 
       context 'when the ticket is solved' do
         before do
-          ticket.status = 'solved'
+          ticket.update(status: 'solved')
         end
 
         it 'opens the ticket' do
-          expect { post_invert_status }.to change(ticket, :status).from('solved').to('open')
+          expect { post_invert_status }.to change { ticket.reload.status }.from('solved').to('open')
         end
       end
     end
@@ -159,7 +153,7 @@ RSpec.describe Brands::TicketsController, type: :controller do
       it 'sets the flash' do
         post_invert_status
 
-        expect(controller).to set_flash[:alert].to('You are not allowed to edit the brand.')
+        expect(controller.flash[:alert]).to eq('You are not allowed to edit the brand.')
       end
 
       it 'redirects the user' do
@@ -169,12 +163,12 @@ RSpec.describe Brands::TicketsController, type: :controller do
   end
 
   describe 'POST refresh' do
-    subject(:post_refresh) { post :refresh, params: { brand_id: brand.id } }
+    subject(:post_refresh) { post "/brands/#{brand.id}/tickets/refresh" }
 
     let(:load_new_tweets_job_class) { class_spy(LoadNewTweetsJob) }
 
     context 'when user is authorized' do
-      let(:user) { FactoryBot.create(:user) }
+      let(:user) { FactoryBot.create(:user, :with_account) }
 
       before do
         brand.users << user
@@ -195,7 +189,7 @@ RSpec.describe Brands::TicketsController, type: :controller do
       it 'sets the flash' do
         post_refresh
 
-        expect(controller).to set_flash[:alert].to('You are not allowed to edit the brand.')
+        expect(controller.flash[:alert]).to eq('You are not allowed to edit the brand.')
       end
 
       it 'redirects the user' do
