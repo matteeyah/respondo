e978f1cb8f33181b7a2cc2a84f09dfaf76f490b2
Matija Čupić <matteeyah@gmail.com>
2019-10-12 18:15:26 +0200

    Change auth helper methods to use post

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe '#auth_twitter_path' do
-    subject(:auth_twitter_path) { helper.auth_twitter_path({}) }
+  describe '#auth_twitter_link' do
+    subject(:auth_twitter_link) { helper.auth_twitter_link('text', 'model') }
 
-    it { is_expected.to start_with('/auth/twitter?') }
+    it { is_expected.to include('/auth/twitter?model=model') }
+    it { is_expected.to include('data-method="post"') }
+    it { is_expected.to include('text') }
   end
 
-  describe '#auth_google_oauth2_path' do
-    subject(:auth_google_oauth2_path) { helper.auth_google_oauth2_path({}) }
+  describe '#auth_google_link' do
+    subject(:auth_google_link) { helper.auth_google_link('text', 'model') }
 
-    it { is_expected.to start_with('/auth/google_oauth2?') }
+    it { is_expected.to include('/auth/google_oauth2?model=model') }
+    it { is_expected.to include('data-method="post"') }
+    it { is_expected.to include('text') }
   end
 end
