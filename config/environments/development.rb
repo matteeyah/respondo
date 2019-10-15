023ee1068fa11b226000ab3210e1b646195cb6ab
Matija Čupić <matteeyah@gmail.com>
2019-10-15 19:52:53 +0200

    Add User#brand to ignored Bullet associations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,5 +82,7 @@ Rails.application.configure do
     # that there is a user associated with the account and this triggers bullet
     # unused_eager_loading for some reason.
     Bullet.add_whitelist type: :unused_eager_loading, class_name: 'Account', association: :user
+    # This doesn't have a Bullet Call Stack when triggering.
+    Bullet.add_whitelist type: :unused_eager_loading, class_name: 'User', association: :brand
   end
 end
