2541ee5b7f62c92f3a6c474306f392c5b7ec4c7b
Matija Čupić <matteeyah@gmail.com>
2019-10-22 19:20:38 +0200

    Refactor navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@
 
 module ApplicationHelper
   def auth_twitter_link(text, model)
-    link_to text, auth_twitter_path(model: model), method: :post
+    link_to text, auth_twitter_path(model: model), method: :post, class: 'nav-link'
   end
 
   def auth_google_link(text, model)
-    link_to text, auth_google_oauth2_path(model: model), method: :post
+    link_to text, auth_google_oauth2_path(model: model), method: :post, class: 'nav-link'
   end
 
   private
