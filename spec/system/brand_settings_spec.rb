bf346d3643f8f0f3c97d77ab59c861d02587dad2
Matija Čupić <matteeyah@gmail.com>
2020-04-07 15:43:41 +0200

    Reorder navigation in brand settings system specs

--- spec/system/brand_settings_spec.rb
+++ spec/system/brand_settings_spec.rb
@@ -14,19 +14,21 @@ RSpec.describe 'Brand settings', type: :system do
 
     sign_in_user
     sign_in_brand(brand)
-
-    click_link 'Brand settings'
   end
 
   it 'allows the user to authorize an account' do
+    click_link 'Brand settings'
+
     add_oauth_mock_for_brand(brand, FactoryBot.create(:brand_account, provider: 'disqus'))
-    click_link('Authorize Disqus', wait: 10)
+    click_link 'Authorize Disqus'
 
     expect(page).to have_link('Remove Disqus')
   end
 
   it 'allows the user to remove an account' do
     FactoryBot.create(:brand_account, provider: 'disqus', brand: brand)
+    click_link 'Brand settings'
+
     click_link 'Remove Disqus'
 
     expect(page).to have_link('Authorize Disqus')
@@ -34,8 +36,9 @@ RSpec.describe 'Brand settings', type: :system do
 
   it 'allows the user to add users to brand' do
     external_user = FactoryBot.create(:user)
+    click_link 'Brand settings'
 
-    select(external_user.name, from: 'add-user', wait: 10)
+    select external_user.name, from: 'add-user'
     click_button 'Add User'
 
     expect(page).to have_link("Remove #{external_user.name}")
@@ -43,13 +46,16 @@ RSpec.describe 'Brand settings', type: :system do
 
   it 'allows the user to remove users from brand' do
     existing_user = FactoryBot.create(:user, brand: brand)
+    click_link 'Brand settings'
 
-    click_link("Remove #{existing_user.name}", wait: 10)
+    click_link "Remove #{existing_user.name}"
 
     expect(page).to have_select('add-user', with_options: [existing_user.name])
   end
 
   it 'allows the user to edit the brand domain' do
+    click_link 'Brand settings'
+
     fill_in 'brand[domain]', with: 'example.com'
     click_button 'Update domain'
 
@@ -57,6 +63,8 @@ RSpec.describe 'Brand settings', type: :system do
   end
 
   it 'prevents the user to update the brand with an invalid domain' do
+    click_link 'Brand settings'
+
     fill_in 'brand[domain]', with: 'invalid!domain.com'
     click_button 'Update domain'
 
