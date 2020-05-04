ef58dbe31212066c0e03fbf10f48e1e084d64c7c
Matija Čupić <matteeyah@gmail.com>
2020-05-04 23:17:58 +0200

    Implement Paddle client

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -10,4 +10,14 @@ class Subscription < ApplicationRecord
   validates :email, presence: { allow_blank: false }
   validates :cancel_url, presence: { allow_blank: false }
   validates :update_url, presence: { allow_blank: false }
+
+  def change_quantity(new_quantity)
+    paddle_client.change_quantity(external_uid, new_quantity)
+  end
+
+  private
+
+  def paddle_client
+    @paddle_client ||= Paddle::Client.new(ENV['PADDLE_VENDOR_ID'], ENV['PADDLE_VENDOR_AUTH'])
+  end
 end
---
+++ lib/paddle/client.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Paddle
+  class Client
+    UPDATE_SUBSCRIPTION_URL = 'https://vendors.paddle.com/api/2.0/subscription/users/update'
+
+    def initialize(vendor_id, vendor_auth_code)
+      @vendor_id = vendor_id
+      @vendor_auth_code = vendor_auth_code
+    end
+
+    def change_quantity(subscription_id, new_quantity)
+      Net::HTTP.post_form(
+        URI(UPDATE_SUBSCRIPTION_URL),
+        vendor_id: @vendor_id,
+        vendor_auth_code: @vendor_auth_code,
+        subscription_id: subscription_id,
+        quantity: new_quantity
+      )
+    end
+  end
+end
