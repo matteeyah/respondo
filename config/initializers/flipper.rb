9e7c0f3c632e7524e02f5f9ce26d9d2339f22611
Matija Čupić <matteeyah@gmail.com>
2020-05-06 00:28:51 +0200

    Add feature flags

---
+++ config/initializers/flipper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Flipper.configure do |config|
+  config.default do
+    # pick an adapter, this uses memory, any will do
+    adapter = Flipper::Adapters::ActiveRecord.new
+
+    # pass adapter to handy DSL instance
+    Flipper.new(adapter)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_160415) do
+ActiveRecord::Schema.define(version: 2020_05_05_221738) do
 
   create_table "authors", force: :cascade do |t|
     t.string "external_uid", null: false
@@ -44,6 +44,22 @@ ActiveRecord::Schema.define(version: 2020_05_04_160415) do
     t.string "domain"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "flipper_features", force: :cascade do |t|
+    t.string "key", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["key"], name: "index_flipper_features_on_key", unique: true
+  end
+
+  create_table "flipper_gates", force: :cascade do |t|
+    t.string "feature_key", null: false
+    t.string "key", null: false
+    t.string "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
   end
 
   create_table "internal_notes", force: :cascade do |t|
