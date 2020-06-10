e91f30510d6381cd88f02ae086828b41f646a1a4
Matija Čupić <matteeyah@gmail.com>
2020-06-10 13:46:38 +0200

    Move back ApplicationController#authorize!

--- app/controllers/users/application_controller.rb
+++ app/controllers/users/application_controller.rb
@@ -8,5 +8,11 @@ module Users
       @user ||= User.find(params[:user_id] || params[:id])
     end
     helper_method :user
+
+    def authorize!
+      return if user == current_user
+
+      redirect_back fallback_location: root_path, flash: { warning: 'You are not allowed to edit the user.' }
+    end
   end
 end
