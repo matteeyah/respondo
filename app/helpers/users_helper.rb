df0cdff6a399b9c4e7069c159b56b5d53701a126
Nikola Obucina <obuciina@gmail.com>
2022-06-21 13:16:52 +0200

    Add Gravatar into user setting

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
-module UserHelper
+module UsersHelper
     def user_gravatar(user)
-      gravatar_id = Digest::MD5::hexdigest(user.eamil.downcase)
+      gravatar_id = Digest::MD5::hexdigest(user.accounts.first.email.downcase)
       gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
-      image_tag(gravatar_url, alt: user.name)
+      image_tag(gravatar_url, alt: user.name, :height => "32px", :width => "32px")
     end
 end
