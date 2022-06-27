b249c4b8b5b391e6e3996e15d4ff0e22462f9f56
Nikola Obucina <obuciina@gmail.com>
2022-06-27 18:38:58 +0200

    fix robocop

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module UsersHelper
-    def user_gravatar(user)
-      gravatar_id = Digest::MD5::hexdigest(user.accounts.first.email.downcase)
-      gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
-      image_tag(gravatar_url, alt: user.name, :height => "32px", :width => "32px")
-    end
+  def user_gravatar(user)
+    gravatar_id = Digest::MD5.hexdigest(user.accounts.first.email.downcase)
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, height: '32px', width: '32px')
+  end
 end
