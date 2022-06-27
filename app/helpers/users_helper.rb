24ddf099464f3f558d2fb827a565431e4fc04bfc
Matija Čupić <matteeyah@gmail.com>
2022-06-27 19:29:13 +0200

    Clean up settings implementation

--- app/helpers/users_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module UsersHelper
-  def user_gravatar(user)
-    gravatar_id = Digest::MD5.hexdigest(user.accounts.first.email.to_s.downcase)
-    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, height: '32px', width: '32px')
-  end
-end
