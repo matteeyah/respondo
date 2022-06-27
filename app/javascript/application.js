772f539854a6579bacddae54754abf84031b965e
Matija Čupić <matteeyah@gmail.com>
2022-06-27 17:22:58 +0200

    Redesign sidebar (again)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,6 @@ import 'popper'
 import 'bootstrap'
 
 import '@hotwired/turbo-rails'
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-tooltip=true]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
