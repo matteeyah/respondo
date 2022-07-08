37d9789f33522fb7eafc159e84f2fe04ef582448
Nikola Obucina <obuciina@gmail.com>
2022-07-08 20:52:25 +0200

    Use values for dashboard widget

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,6 +11,11 @@ class HomeController < ApplicationController
     @brand = current_brand
     @user = current_user
     @newest = current_brand&.tickets&.order(created_at: :desc)&.first(3)
+    @total_open = current_brand&.tickets&.select { |e| e.status == 'open' }.count
+    @total_resolved = current_brand&.tickets&.select { |e| e.status == 'solved' }.count
+    @total_new = current_brand&.tickets&.select do |e|
+      e.created_at.strftime('%d %b %y') == Time.zone.now.strftime('%d %b %y') && e.status == 'open'
+    end.count
   end
 
   private
