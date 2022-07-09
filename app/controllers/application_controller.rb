bbe42db5739ea2fdf9e5f97b9c90e526b9e86d25
Matija Čupić <matteeyah@gmail.com>
2022-07-09 14:54:17 +0200

    Add etag for importmap cache

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  etag { Rails.application.importmap.digest(resolver: helpers) if request.format&.html? }
+
   protect_from_forgery with: :exception
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
