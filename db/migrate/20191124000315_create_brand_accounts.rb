7064e7e867c56cb4adc4fedc9737bce28e54cb0e
Matija Čupić <matteeyah@gmail.com>
2022-07-13 13:12:39 +0200

    Bump existing migrations to version 7

--- db/migrate/20191124000315_create_brand_accounts.rb
+++ db/migrate/20191124000315_create_brand_accounts.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateBrandAccounts < ActiveRecord::Migration[6.0]
+class CreateBrandAccounts < ActiveRecord::Migration[7.0]
   def change
     create_table :brand_accounts do |t|
       t.string :external_uid, null: false
