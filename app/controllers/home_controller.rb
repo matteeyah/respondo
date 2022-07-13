554477a1f34234898042d689ee211af87b8bde4b
Nikola Obucina <obuciina@gmail.com>
2022-07-13 09:42:52 +0200

    Fix rubocop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@
 
 class HomeController < ApplicationController
   before_action :authenticate!, only: :index
+  before_action :newest, only: :index
+  before_action :newest_count, only: :index
+  before_action :tickets_overview_open, only: :index
+  before_action :tickets_overview_resolved, only: :index
 
   def login
     render layout: false
@@ -10,15 +14,27 @@ class HomeController < ApplicationController
   def index
     @brand = current_brand
     @user = current_user
+  end
+
+  private
+
+  def newest
     @newest = current_brand&.tickets&.order(created_at: :desc)&.first(3)
+  end
+
+  def tickets_overview_open
     @total_open = current_brand&.tickets&.select { |e| e.status == 'open' }&.count
+  end
+
+  def tickets_overview_resolved
     @total_resolved = current_brand&.tickets&.select { |e| e.status == 'solved' }&.count
+  end
+
+  def newest_count
     @total_new = current_brand&.tickets&.select do |e|
       e.created_at.strftime('%d %b %y') == Time.zone.now.strftime('%d %b %y') && e.status == 'open'
     end&.count
   end
-
-  private
 
   def authenticate!
     return unless current_user.nil?
