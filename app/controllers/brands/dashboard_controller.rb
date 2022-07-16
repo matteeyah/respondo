3f8a884006b942c52aab0138ffa3b074bc4c7fc3
Matija Čupić <matteeyah@gmail.com>
2022-07-16 12:39:10 +0200

    Touch up ticket dashboard

--- app/controllers/brands/dashboard_controller.rb
+++ app/controllers/brands/dashboard_controller.rb
@@ -5,11 +5,12 @@ module Brands
     include Pundit::Authorization
 
     def index
-      authorize(brand, policy_class: TicketPolicy)
-      @newest = newest_tickets.take(3)
+      authorize(brand, :user_in_brand?)
+
+      @newest_tickets = newest_tickets
+      @new_count = new_tickets.count
       @open_count = current_brand.tickets.open.count
       @solved_count = current_brand.tickets.solved.count
-      @new_count = new_tickets.count
     end
 
     private
@@ -19,7 +20,7 @@ module Brands
     end
 
     def newest_tickets
-      current_brand.tickets.order(created_at: :desc)
+      current_brand.tickets.order(created_at: :desc).take(3)
     end
   end
 end
--- spec/system/dashboard_spec.rb
+++ spec/system/dashboard_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require './spec/support/sign_in_out_system_helpers'
-require './spec/support/system/allows_interacting_with_tickets_examples'
 
 RSpec.describe 'Dashboard', type: :system do
   include SignInOutSystemHelpers
@@ -9,39 +8,37 @@ RSpec.describe 'Dashboard', type: :system do
   let(:brand) { create(:brand, :with_account) }
   let!(:tickets) { create_list(:internal_ticket, 2, brand:).map(&:base_ticket) }
 
-  before do
-    create(:subscription, brand:)
-
-    visit '/'
-  end
-
   it 'shows the newest tickets' do
+    visit '/'
+
     sign_in_user
     sign_in_brand(brand)
     click_link('Dashboard')
 
-    expect(page).to have_text(tickets.first&.content)
-    expect(page).to have_text(tickets.first&.author&.username)
+    expect(page).to have_text(tickets.first.content)
+    expect(page).to have_text(tickets.first.author.username)
 
-    expect(page).to have_text(tickets.second&.content)
-    expect(page).to have_text(tickets.second&.author&.username)
+    expect(page).to have_text(tickets.second.content)
+    expect(page).to have_text(tickets.second.author.username)
   end
 
   it 'shows the tickets info widgets' do
+    visit '/'
+
     sign_in_user
     sign_in_brand(brand)
     click_link('Dashboard')
-
-    expect(page).to have_text('Feel free to hop into the brand tickets!')
 
     expect(page).to have_text('New Tickets')
     expect(page).to have_text(tickets.count)
 
     expect(page).to have_text('Total Open')
-    expect(page).to have_text(tickets&.select { |e| e.status == 'open' }&.count)
+    expect(page).to have_text(tickets.count)
   end
 
   it 'allows show all tickets with home widget' do
+    visit '/'
+
     sign_in_user
     sign_in_brand(brand)
     click_link('Dashboard')
