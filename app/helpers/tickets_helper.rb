bf3d8f11906c7ec52d15ff9d2e8af90a948bca9a
Matija Čupić <matteeyah@gmail.com>
2022-07-16 12:08:33 +0200

    Remove redundant invert_status helpers

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,24 +1,6 @@
 # frozen_string_literal: true
 
 module TicketsHelper
-  def invert_status_action(status)
-    case status
-    when 'open'
-      bi_icon('check-circle-fill', 'text-primary fs-4')
-    when 'solved'
-      bi_icon('backspace-fill', 'text-primary fs-4')
-    end
-  end
-
-  def invert_status_action_text(status)
-    case status
-    when 'open'
-      'Solve'
-    when 'solved'
-      'Open'
-    end
-  end
-
   def ticket_author_header(user_authorized, ticket)
     author_link = link_to(ticket.author.username, ticket.author.external_link)
     if user_authorized && ticket.creator
