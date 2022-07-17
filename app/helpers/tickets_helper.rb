d8e5666458eb9e152dde1016a26e3b655474a743
Nikola Obucina <obuciina@gmail.com>
2022-07-17 14:40:19 +0200

    Redesign ticket header and footer

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -2,9 +2,18 @@
 
 module TicketsHelper
   def ticket_author_header(user_authorized, ticket)
-    author_link = link_to(ticket.author.username, ticket.author.external_link)
+    author_link = link_to(ticket.author.username, ticket.author.external_link, class: 'text-decoration-none')
     if user_authorized && ticket.creator
       "#{ticket.creator.name} as #{author_link}"
+    else
+      author_link
+    end
+  end
+
+  def ticket_provider_as_author_link(user_authorized, ticket)
+    author_link = link_to(ticket.author.username, ticket.author.external_link)
+    if user_authorized && ticket.creator
+      "#{ticket.actual_provider} as #{author_link}"
     else
       author_link
     end
