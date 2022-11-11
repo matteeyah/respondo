47fb643d58dae3ad2863dfe9e07c1160ff4be46c
Matija Čupić <matteeyah@gmail.com>
2022-11-11 21:34:02 +0100

    Tweak system test config

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,8 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by(:selenium, using: :headless_chrome, screen_size: [1920, 1080]) do |driver_options|
+    # headless chrome can't run in Docker with sandboxing
+    driver_options.add_argument('no-sandbox')
+  end
 end
