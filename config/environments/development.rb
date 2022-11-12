bbbc2587c3bfc1b3ef0efa3fd3078ba6da604ef8
Matija Čupić <matteeyah@gmail.com>
2022-11-12 01:47:26 +0100

    Set up queue adapters

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Use a real queuing backend for Active Job (and separate queues per environment).
+  config.active_job.queue_adapter = :resque
+  # config.active_job.queue_name_prefix = "respondo_production"
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -49,4 +49,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Use the test adapter for ActiveJob
+  config.active_job.queue_adapter = :test
 end
