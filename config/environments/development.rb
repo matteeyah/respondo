ee64c16f0cb57f9bb8e6b1fa2ad19caf13d17fe2
Matija Čupić <matteeyah@gmail.com>
2022-11-12 01:52:06 +0100

    Fix ActiveJob queue name quirk

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,5 +64,5 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "respondo_production"
+  # config.active_job.queue_name_prefix = "respondo_development"
 end
