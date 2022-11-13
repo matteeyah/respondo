fd0fae3aecaf45482c0533620312211a9888dd24
Matija Čupić <matteeyah@gmail.com>
2022-11-13 19:10:10 +0100

    Migrate internal note tests to Minitest

---
+++ test/models/internal_note_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class InternalNoteTest < ActiveSupport::TestCase
+  test 'validates presence of content' do
+    internal_note = internal_notes(:default)
+    internal_note.content = nil
+
+    assert_not internal_note.save
+  end
+end
