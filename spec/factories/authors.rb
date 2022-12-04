3510512b02dfaebc97ffdae9758c360fd916b294
Matija Čupić <matteeyah@gmail.com>
2022-12-04 23:27:15 +0100

    Remove unused gems

--- spec/factories/authors.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :author do
-    external_uid { generate(:external_uid) }
-    username { Faker::Internet.username }
-    provider { Author.providers.keys.sample }
-  end
-end
