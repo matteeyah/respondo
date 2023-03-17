5d3a032c462506f81d681d9a673cb508c497a5d6
Matija Čupić <matteeyah@gmail.com>
2023-03-17 18:15:09 +0100

    Add tests for inbound mailbox

--- app/mailboxes/inbound_mailbox.rb
+++ app/mailboxes/inbound_mailbox.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class InboundMailbox < ApplicationMailbox
+  before_processing :organization
+
   def process
-    binding.break
     Ticket.create!(
       external_uid: mail.message_id, content: mail.body.to_s, created_at: mail.date,
       parent:, organization:, creator: nil, author: sender,
@@ -15,7 +16,7 @@ class InboundMailbox < ApplicationMailbox
   def organization
     @organization ||= Organization.find(mail.to.first.match(INBOUND_REGEX).captures.first)
   rescue StandardError
-    bounce_with # TODO
+    bounce_with BounceMailer.no_organization(inbound_email, sender)
   end
 
   def parent
@@ -23,6 +24,6 @@ class InboundMailbox < ApplicationMailbox
   end
 
   def sender
-    @sender ||= Author.find_or_create_by(username: mail.from.first)
+    @sender ||= Author.find_or_create_by(external_uid: mail.from.first, username: mail.from.first, provider: :email)
   end
 end
--- test/mailboxes/inbound_mailbox_test.rb
+++ test/mailboxes/inbound_mailbox_test.rb
@@ -3,11 +3,35 @@
 require 'test_helper'
 
 class InboundMailboxTest < ActionMailbox::TestCase
-  test 'receive mail' do
-    receive_inbound_email_from_mail \
-      to: '"someone" <inbound+4@mail.respondohub.com>',
-      from: '"else" <inbound+4@mail.respondohub.com>',
-      subject: 'Hello world!',
-      body: 'Hello?'
+  include ActionMailer::TestHelper
+
+  test 'does not create tickets when organization is missing' do
+    assert_no_difference -> { Ticket.count } do
+      receive_inbound_email_from_mail \
+        to: '"respondo" <inbound+999@mail.respondohub.com>',
+        from: '"someone" <someone@example.com>',
+        subject: 'Hello world!',
+        body: 'Hello?'
+    end
+  end
+
+  test 'bounces mail when organization is missing' do
+    assert_enqueued_emails(1) do
+      receive_inbound_email_from_mail \
+        to: '"respondo" <inbound+999@mail.respondohub.com>',
+        from: '"someone" <someone@example.com>',
+        subject: 'Hello world!',
+        body: 'Hello?'
+    end
+  end
+
+  test 'creates ticket when organization is right' do
+    assert_difference -> { Ticket.count }, 1 do
+      receive_inbound_email_from_mail \
+        to: "\"respondo\" <inbound+#{organizations(:respondo).id}@mail.respondohub.com>",
+        from: '"someone" <someone@example.com>',
+        subject: 'Hello world!',
+        body: 'Hello?'
+    end
   end
 end
