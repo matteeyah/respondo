2085e055dca37088de58367dc5a748f951cfc7be
Matija Čupić <matteeyah@gmail.com>
2023-03-21 20:21:05 +0100

    Add tests for AI completion

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-unless Rails.env.test?
-  OpenAI.configure do |config|
-    config.access_token = Rails.application.credentials.openai.access_token
-    config.organization_id = Rails.application.credentials.openai.organization_id
-  end
+OpenAI.configure do |config|
+  config.access_token = Rails.application.credentials.openai.access_token
+  config.organization_id = Rails.application.credentials.openai.organization_id
 end
