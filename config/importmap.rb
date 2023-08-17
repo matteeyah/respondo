6ec8c9cfbce712595b731c6fe76d2645e2fee2a4
Matija Čupić <matteeyah@gmail.com>
2023-08-17 17:44:13 +0200

    Bump importmap dependencies

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,7 +7,7 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 
-pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js'
-pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js'
+pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js'
+pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.3.1/dist/js/bootstrap.esm.js'
 
 pin_all_from 'app/javascript/controllers', under: 'controllers'
