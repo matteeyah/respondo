5c5baa6d8e53d990006eccff3c5889d28a3f468a
Vuk <nikolicm.vuk@gmail.com>
2023-10-06 18:37:38 +0200

    test fixes

--- lib/clients/linkedin.rb
+++ lib/clients/linkedin.rb
@@ -1,12 +1,4 @@
 # frozen_string_literal: true
 
-module Clients
-  class Linkedin < Clients::ProviderClient
-    def initialize(client_id, client_secret)
-      super()
-
-      @client_id = client_id
-      @client_secret = client_secret
-    end
-  end
+class Linkedin
 end
