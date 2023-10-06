4569ce42f9cff52457716e888a44888e72cee1d7
Vuk <nikolicm.vuk@gmail.com>
2023-10-06 18:10:33 +0200

    fix test 3

--- lib/clients/linkedin.rb
+++ lib/clients/linkedin.rb
@@ -2,5 +2,11 @@
 
 module Clients
   class Linkedin < Clients::ProviderClient
+    def initialize(client_id, client_secret)
+      super()
+
+      @client_id = client_id
+      @client_secret = client_secret
+    end
   end
 end
