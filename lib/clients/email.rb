79f3e18359fef9dc6e1156103ca7ca3acdb35084
Vuk <nikolicm.vuk@gmail.com>
2023-11-06 15:04:16 +0100

    - resolving CR comments 2

--- lib/clients/email.rb
+++ lib/clients/email.rb
@@ -17,7 +17,7 @@ module Clients
         external_uid: mail.message_id, content: response_text, created_at: mail.date,
         author: { external_uid: mail.from.first, username: mail.from.first },
         ticketable_attributes: { reply_to: mail.reply_to.first, subject: mail.subject },
-        parent_uid: mail.in_reply_to, external_link: 'mail.google.com'
+        parent_uid: mail.in_reply_to, external_link: 'https://mail.google.com/uid_1'
       }
     end
 
