11ed27657a3b716137d7e71061f85365eaa6d9fc
Matija Čupić <matteeyah@gmail.com>
2023-11-22 20:05:37 +0100

    Add ad generation logic

--- test/jobs/generate_ad_job_test.rb
+++ test/jobs/generate_ad_job_test.rb
@@ -7,15 +7,21 @@ class GenerateAdJobTest < ActiveJob::TestCase
 
   setup do
     @guid = SecureRandom.uuid
-  end
 
-  test 'broadcasts ad output' do
     stub_request(:get, 'https://api.twitter.com/2/users/uid_1/tweets')
       .to_return(
         status: 200, headers: { 'Content-Type' => 'application/json; charset=utf-8' },
         body: file_fixture('x_posts.json').read
       )
 
+    stub_request(:post, 'https://api.openai.com/v1/chat/completions')
+      .to_return(
+        status: 200, body: file_fixture('openai_chat.json'),
+        headers: { 'Content-Type' => 'application/json' }
+      )
+  end
+
+  test 'broadcasts ad output' do
     assert_turbo_stream_broadcasts([@guid, :ad], count: 1) do
       GenerateAdJob.perform_now(@guid, 'Analyze the results.', authors(:james))
     end
