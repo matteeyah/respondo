89ccb1347b810191a73818b3648b407e34479e0d
Vuk <nikolicm.vuk@gmail.com>
2023-11-23 15:22:04 +0100

    [Feature] Implement product and CRUD for it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_08_161259) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_102041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -96,6 +96,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_08_161259) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["domain"], name: "index_organizations_on_domain", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.bigint "organization_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["organization_id"], name: "index_products_on_organization_id"
   end
 
   create_table "tags", force: :cascade do |t|
