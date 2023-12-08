2b689d5407db16cf5cbce3b1a5748d17b975026c
Matija Čupić <matteeyah@gmail.com>
2023-12-08 20:04:47 +0100

    Nicer ads page (#383)
    * Bump ruby dependencies
    
    * Improve ad page design
    
    * Fix tests
    
    * Bump ruby docs dependencies

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 class AdsController < ApplicationController
+  before_action :set_product, only: :create
+
   def new; end
 
   def create
     @guid = SecureRandom.uuid
-    GenerateAdJob.perform_later(@guid, params[:product_description], *Author.where(id: params[:author_ids]))
+    GenerateAdJob.perform_later(@guid, @product.description, *Author.where(id: params[:author_ids]))
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:product_id])
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,7 +7,11 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 
+# Bootstrap
 pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js'
 pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js'
+
+# Tom-Select - searchable select
+pin 'tom-select', to: 'https://ga.jspm.io/npm:tom-select@2.3.1/dist/js/tom-select.base.js'
 
 pin_all_from 'app/javascript/controllers', under: 'controllers'
