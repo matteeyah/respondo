f7ea774435d17b04d53904497531a2de3907015b
Matija Čupić <matteeyah@gmail.com>
2025-03-28 19:17:17 +0100

    Switch to SQLite

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2023_12_20_132413) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-
+ActiveRecord::Schema[8.0].define(version: 2023_11_23_102041) do
   create_table "assignments", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "mention_id", null: false
+    t.integer "user_id", null: false
+    t.integer "mention_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["mention_id"], name: "index_assignments_on_mention_id", unique: true
@@ -28,7 +25,7 @@ ActiveRecord::Schema[8.0].define(version: 2023_12_20_132413) do
     t.string "username", null: false
     t.string "external_link", null: false
     t.integer "provider", null: false
-    t.bigint "organization_id", null: false
+    t.integer "organization_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["external_uid", "provider", "organization_id"], name: "index_authors_on_external_uid_and_provider_and_organization_id", unique: true
@@ -37,8 +34,8 @@ ActiveRecord::Schema[8.0].define(version: 2023_12_20_132413) do
 
   create_table "internal_notes", force: :cascade do |t|
     t.text "content", null: false
-    t.bigint "creator_id", null: false
-    t.bigint "mention_id", null: false
+    t.integer "creator_id", null: false
+    t.integer "mention_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["creator_id"], name: "index_internal_notes_on_creator_id"
@@ -46,8 +43,8 @@ ActiveRecord::Schema[8.0].define(version: 2023_12_20_132413) do
   end
 
   create_table "mention_tags", force: :cascade do |t|
-    t.bigint "mention_id", null: false
-    t.bigint "tag_id", null: false
+    t.integer "mention_id", null: false
+    t.integer "tag_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["mention_id", "tag_id"], name: "index_mention_tags_on_mention_id_and_tag_id", unique: true
@@ -60,11 +57,11 @@ ActiveRecord::Schema[8.0].define(version: 2023_12_20_132413) do
     t.text "content", null: false
     t.integer "status", default: 0, null: false
     t.string "external_link", null: false
-    t.bigint "organization_id", null: false
-    t.bigint "author_id", null: false
-    t.bigint "creator_id"
-    t.bigint "parent_id"
-    t.bigint "source_id", null: false
+    t.integer "organization_id", null: false
+    t.integer "author_id", null: false
+    t.integer "creator_id"
+    t.integer "parent_id"
+    t.integer "source_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_mentions_on_author_id"
@@ -82,7 +79,7 @@ ActiveRecord::Schema[8.0].define(version: 2023_12_20_132413) do
     t.integer "provider", null: false
     t.string "token"
     t.string "secret"
-    t.bigint "organization_id", null: false
+    t.integer "organization_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["external_uid", "provider"], name: "index_organization_accounts_on_external_uid_and_provider", unique: true
@@ -101,110 +98,10 @@ ActiveRecord::Schema[8.0].define(version: 2023_12_20_132413) do
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
-    t.bigint "organization_id", null: false
+    t.integer "organization_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["organization_id"], name: "index_products_on_organization_id"
-  end
-
-  create_table "solid_cache_entries", force: :cascade do |t|
-    t.binary "key", null: false
-    t.binary "value", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_solid_cache_entries_on_key", unique: true
-  end
-
-  create_table "solid_queue_blocked_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.string "queue_name", null: false
-    t.integer "priority", default: 0, null: false
-    t.string "concurrency_key", null: false
-    t.datetime "expires_at", null: false
-    t.datetime "created_at", null: false
-    t.index ["expires_at", "concurrency_key"], name: "index_solid_queue_blocked_executions_for_maintenance"
-    t.index ["job_id"], name: "index_solid_queue_blocked_executions_on_job_id", unique: true
-  end
-
-  create_table "solid_queue_claimed_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.bigint "process_id"
-    t.datetime "created_at", null: false
-    t.index ["job_id"], name: "index_solid_queue_claimed_executions_on_job_id", unique: true
-    t.index ["process_id", "job_id"], name: "index_solid_queue_claimed_executions_on_process_id_and_job_id"
-  end
-
-  create_table "solid_queue_failed_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.text "error"
-    t.datetime "created_at", null: false
-    t.index ["job_id"], name: "index_solid_queue_failed_executions_on_job_id", unique: true
-  end
-
-  create_table "solid_queue_jobs", force: :cascade do |t|
-    t.string "queue_name", null: false
-    t.string "class_name", null: false
-    t.text "arguments"
-    t.integer "priority", default: 0, null: false
-    t.string "active_job_id"
-    t.datetime "scheduled_at"
-    t.datetime "finished_at"
-    t.string "concurrency_key"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["active_job_id"], name: "index_solid_queue_jobs_on_active_job_id"
-    t.index ["class_name"], name: "index_solid_queue_jobs_on_class_name"
-    t.index ["finished_at"], name: "index_solid_queue_jobs_on_finished_at"
-    t.index ["queue_name", "finished_at"], name: "index_solid_queue_jobs_for_filtering"
-    t.index ["scheduled_at", "finished_at"], name: "index_solid_queue_jobs_for_alerting"
-  end
-
-  create_table "solid_queue_pauses", force: :cascade do |t|
-    t.string "queue_name", null: false
-    t.datetime "created_at", null: false
-    t.index ["queue_name"], name: "index_solid_queue_pauses_on_queue_name", unique: true
-  end
-
-  create_table "solid_queue_processes", force: :cascade do |t|
-    t.string "kind", null: false
-    t.datetime "last_heartbeat_at", null: false
-    t.bigint "supervisor_id"
-    t.integer "pid", null: false
-    t.string "hostname"
-    t.text "metadata"
-    t.datetime "created_at", null: false
-    t.index ["last_heartbeat_at"], name: "index_solid_queue_processes_on_last_heartbeat_at"
-    t.index ["supervisor_id"], name: "index_solid_queue_processes_on_supervisor_id"
-  end
-
-  create_table "solid_queue_ready_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.string "queue_name", null: false
-    t.integer "priority", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.index ["job_id"], name: "index_solid_queue_ready_executions_on_job_id", unique: true
-    t.index ["priority", "job_id"], name: "index_solid_queue_poll_all"
-    t.index ["queue_name", "priority", "job_id"], name: "index_solid_queue_poll_by_queue"
-  end
-
-  create_table "solid_queue_scheduled_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.string "queue_name", null: false
-    t.integer "priority", default: 0, null: false
-    t.datetime "scheduled_at", null: false
-    t.datetime "created_at", null: false
-    t.index ["job_id"], name: "index_solid_queue_scheduled_executions_on_job_id", unique: true
-    t.index ["scheduled_at", "priority", "job_id"], name: "index_solid_queue_dispatch_all"
-  end
-
-  create_table "solid_queue_semaphores", force: :cascade do |t|
-    t.string "key", null: false
-    t.integer "value", default: 1, null: false
-    t.datetime "expires_at", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["expires_at"], name: "index_solid_queue_semaphores_on_expires_at"
-    t.index ["key", "value"], name: "index_solid_queue_semaphores_on_key_and_value"
-    t.index ["key"], name: "index_solid_queue_semaphores_on_key", unique: true
   end
 
   create_table "tags", force: :cascade do |t|
@@ -218,7 +115,7 @@ ActiveRecord::Schema[8.0].define(version: 2023_12_20_132413) do
     t.string "external_uid", null: false
     t.string "email"
     t.integer "provider", null: false
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id", "provider"], name: "index_user_accounts_on_user_id_and_provider", unique: true
@@ -229,16 +126,11 @@ ActiveRecord::Schema[8.0].define(version: 2023_12_20_132413) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "organization_id"
+    t.integer "organization_id"
     t.index ["organization_id"], name: "index_users_on_organization_id"
   end
 
   add_foreign_key "authors", "organizations"
   add_foreign_key "mention_tags", "mentions"
   add_foreign_key "mention_tags", "tags"
-  add_foreign_key "solid_queue_blocked_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "solid_queue_claimed_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "solid_queue_failed_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "solid_queue_ready_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "solid_queue_scheduled_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
 end
