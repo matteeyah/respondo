fc2cd31c57c46962ddf8147a563933fb60596446
Matija Čupić <matteeyah@gmail.com>
2025-03-29 12:14:57 +0100

    Clean unused parts (#393)
    * Remove unused styling
    
    * Remove unused Postgres service
    
    * Remove unused templates

--- .github/pull_request_template.md
+++
@@ -1,9 +0,0 @@
-# Summary
-
-## Checklist
-
-* [ ] Did you follow the guidelines in our Contributing document?
-* [ ] Did you check to ensure there aren't other open [Pull Requests](../../../pulls) for the same update/change?
-
-* [ ] Did you add an explanation of what your changes do?
-* [ ] Did you add unit, integration and system tests if applicable?
--- config/importmap.rb
+++ config/importmap.rb
@@ -11,7 +11,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.3/dist/js/bootstrap.esm.js"
 
-# Tom-Select - searchable select
-pin "tom-select", to: "https://ga.jspm.io/npm:tom-select@2.4.3/dist/js/tom-select.base.js"
-
 pin_all_from "app/javascript/controllers", under: "controllers"
