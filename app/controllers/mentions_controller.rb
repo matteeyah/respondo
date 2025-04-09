adfbf3b0be40144a65b6d973c69f663156d15737
Matija Čupić <matteeyah@gmail.com>
2025-04-09 20:31:50 +0200

    Fix tests

--- app/controllers/mentions_controller.rb
+++ app/controllers/mentions_controller.rb
@@ -8,7 +8,7 @@ class MentionsController < Mentions::ApplicationController
     { organization: [ :users ], internal_notes: [ :creator ] }
   ].freeze
 
-  before_action :set_mention, only: %i[show update destroy permalink]
+  before_action :set_mention, only: %i[show update destroy]
 
   def index
     set_page_and_extract_portion_from(mentions)
